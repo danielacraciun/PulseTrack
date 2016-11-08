a4535f6c212b4bb2d294227ce65ecb9a947df508
Dana <craciundaniela19@gmail.com>
2016-11-08 13:23:29 +0200

    Added insertion field and navigation buttons

--- PulseTrack/addlog.js
+++ PulseTrack/addlog.js
@@ -12,4 +12,29 @@ export class AddItem extends Component {
     super(props);
     this.state = { text: '<pulse value>' };
   }
+  addItem() {
+    // todo: error message if empty fields
+    // Saving is done here
+    // if (this.state.text !== '')
+    // {
+    //
+    // }
+    // Then return to first page after inserting
+    this.props.navigator.pop();
+  }
+
+  render() {
+    return(
+      <View>
+        <Text>LOG PULSE</Text>
+        <View>
+          <TextInput value={this.state.text} onFocus={() => this.setState({text: ''})} onChangeText={(text) => this.setState({text})}/>
+        </View>
+        <View>
+          <Button name='RETURN' onPress={() => this.props.navigator.pop()}/>
+          <Button name='OK' onPress={() => this.addItem()}/>
+        </View>
+      </View>
+    );
+  }
 }
