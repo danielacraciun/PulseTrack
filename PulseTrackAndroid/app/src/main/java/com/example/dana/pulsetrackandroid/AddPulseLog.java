3c54168dce8c0ec3a04bd436997e8f9afa4c1ca4
Dana <craciundaniela19@gmail.com>
2016-11-08 22:02:18 +0200

    Android initialization

---
+++ PulseTrackAndroid/app/src/main/java/com/example/dana/pulsetrackandroid/AddPulseLog.java
@@ -0,0 +1,13 @@
+package com.example.dana.pulsetrackandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPulseLog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_pulse_log);
+    }
+}
---
+++ PulseTrackAndroid/app/src/main/java/com/example/dana/pulsetrackandroid/PulseLog.java
@@ -0,0 +1,8 @@
+package com.example.dana.pulsetrackandroid;
+
+/**
+ * Created by dana on 11/8/16.
+ */
+
+public class PulseLog {
+}
